be0142f903857db71644c8e54261199b96a0ad0b
Eduardo Neves <ematheusneves13@gmail.com>
2022-09-05 11:13:56 -0300

    Foi realizado a primeira insercao via banco, usando o select para mostrar os horarios agendados na view index.php

--- classes/reservas.class.php
+++ classes/reservas.class.php
@@ -1,7 +1,26 @@
 <?php
 
     class Reservas {
+
+        private $conn;
+
+        public function __construct($conn) {
+        $this->conn = $conn;
+
+    }
         
+        public function getReservas() {
+            $array = array();
+
+            $stmt = "SELECT * FROM reservas";
+            $stmt = $this->conn->query($stmt);
+
+            if($stmt->rowCount() > 0) {
+                $array = $stmt->fetchAll();
+            }
+
+            return $array;
+        }
     }
 
 ?>
--- index.php
+++ index.php
@@ -1,8 +1,20 @@
 <?php
 
+
+
 require 'config.php';
-require 'classes/reservas.'
+require 'classes/reservas.class.php';
+
+//Injeção de dependecia
+$reservas = new Reservas($conn);
 
 ?>
 
 <h1>RESERVAS</h1>
+<?php
+    
+    $lista = $reservas->getReservas();
+
+    foreach($lista as $item) {
+        echo $item['pessoa'] . ' reservou a sala de reunião na data ' . $item['data_inicio'] . ' no hoario das ' . $item['horario_inicio'] . ' ás ' . $item['horario_de_fim'];
+    }
